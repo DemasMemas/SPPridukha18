e4a001a19aecae90e093b4d0f5cdb9aaa54c3d41
Dmitriy Pridukha <57756987+DemasMemas@users.noreply.github.com>
2021-11-24 10:42:58 +0300

    Added JavaDoc

---
+++ MusicAndPictureDownload/JavaDoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"pridukha","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"pridukha","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
